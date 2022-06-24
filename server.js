87fe5b9b778a528700770e325ab4969ea26fe2ca
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-06-24 18:22:05 -0400

    added express

---
+++ server.js
@@ -0,0 +1,9 @@
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+ 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
